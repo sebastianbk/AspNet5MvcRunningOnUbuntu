4ebfbcc70bc3222584f31b232df10c37d6b935c2
Sebastian Brandes Kraaijenzank <sb@sebastianbrandes.com>
2015-02-19 16:20:15 +0100

    Initial solution

---
+++ src/AspNet5MvcRunningOnUbuntu/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using AspNet5MvcRunningOnUbuntu.Models;
+using Microsoft.AspNet.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AspNet5MvcRunningOnUbuntu.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            var child = new Child
+            {
+                Name = "Sebastian",
+                MessageToMom = "Look, Mom! I'm running ASP.NET 5 on Ubuntu!"
+            };
+
+            return View(child);
+        }
+    }
+}
---
+++ src/AspNet5MvcRunningOnUbuntu/Models/Child.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace AspNet5MvcRunningOnUbuntu.Models
+{
+    public class Child
+    {
+        public string Name { get; set; }
+        public string MessageToMom { get; set; }
+    }
+}
